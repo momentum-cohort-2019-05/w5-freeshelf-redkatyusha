2158e8c3c17afa478b71f6fdbaa70c3982ea5335
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-25 09:07:01 -0400

    Create Book model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Book(models.Model):
+    """
+    A model for each book in the library.
+    """
+
+    title = models.CharField(max_length=500, help_text='Enter the full title of the book.')
+
+    def get_absolute_url(self):
+        """Returns the URL to access a specific instance of Book."""
+        return reverse('book-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing Book."""
+        return self.title
