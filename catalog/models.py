e8842692c1991500a932f34ff4266ab221086a62
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-27 15:26:20 -0400

    Make new things

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Category(models.Model):
+    
+    name = models.CharField(max_length=200)
+
+    def str(self):
+        return self.name
+
+class Book(models.Model):
+    
+    title = models.CharField(max_length=500)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000)
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
