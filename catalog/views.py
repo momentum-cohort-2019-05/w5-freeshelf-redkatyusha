30603c1df0c9581c2c230d5d9099e5937c4cffc9
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-28 11:08:30 -0400

    Add working book list page

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 
@@ -12,3 +13,6 @@ def index(request):
     }
 
     return render(request, "index.html", context=context)
+
+class BookListView(generic.ListView):
+    model = Book
