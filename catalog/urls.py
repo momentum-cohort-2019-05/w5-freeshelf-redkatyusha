6fe7d5a4426502e563952674a97081142048e1d5
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-28 14:23:17 -0400

    Added catalog detail pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("books/", views.BookListView.as_view(), name="books"),
     path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
     path("categories/", views.CategoryListView.as_view(), name="categories"),
-    path("category/<int:pk>", views.CategoryListView.as_view(), name="category-detail"),
+    path("category/<int:pk>", views.CategoryDetailView.as_view(), name="category-detail"),
 ]
